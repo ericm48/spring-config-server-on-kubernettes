9305350a17fe9ec42b822cf3e85dfe66739485f6
ericm48 <me_manley@yahoo.com>
2025-05-25 18:42:05 -0600

    -Cleanup a few things

--- scripts/buildAndPushImages.sh
+++ scripts/buildAndPushImages.sh
@@ -6,8 +6,9 @@ m3ast #mvn clean package
 docker image rm -f ericm24/config-consumer-service:k8
 docker image build -t ericm24/config-consumer-service:k8 .
 docker push ericm24/config-consumer-service:k8
-cd ..
-cd config-service/
+
+
+cd ../config-service/
 m3ast #mvn clean package
 
 docker image rm -f ericm24/config-service:k8
