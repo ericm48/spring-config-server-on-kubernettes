f19c4d6ec3847f170efa71f52dd72c0e5683e8e3
ericm48 <me_manley@yahoo.com>
2025-06-16 14:08:17 -0600

    -Consumer now works will start on 8080. ConfigServer has 2x dependancy.

--- scripts/buildAndPushImages.sh
+++ scripts/buildAndPushImages.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-cd ../config-consumer-service/
+cd ../config-consumer-service
 m3ast #mvn clean package
 
-docker image rm -f ericm24/config-consumer-service:k8
-docker image build -t ericm24/config-consumer-service:k8 .
-#docker push ericm24/config-consumer-service:k8
+docker image rm -f ericm24/config-consumer-service:k8.1
+docker image build -t ericm24/config-consumer-service:k8.1 .
+docker push ericm24/config-consumer-service:k8.1
 
-mkImagePush
+# Gets messed up with .envrc  Send params...
+#mkImagePush config-consumer-service:k8.1 ericm24
 
-
-cd ../config-service/
+cd ../config-consumer-service
 m3ast #mvn clean package
 
-docker image rm -f ericm24/config-service:k8
-docker image build -t ericm24/config-service:k8 .
-#docker push ericm24/config-service:k8
+docker image rm -f ericm24/config-service:k8.1
+docker image build -t ericm24/config-service:k8.1 .
+docker push ericm24/config-service:k8.1
 
-mkImagePush
+# Gets messed up with .envrc  Send params...
+#mkImagePush config-service:k8.1 ericm24
+
