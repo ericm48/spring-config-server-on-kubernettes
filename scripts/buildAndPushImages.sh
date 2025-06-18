0939d948a31cdfdfe7c7e4c96b90c610e25ce679
ericm48 <me_manley@yahoo.com>
2025-06-18 14:28:00 -0600

    -Incremental commit. Add img build, all works.

--- scripts/buildAndPushImages.sh
+++ scripts/buildAndPushImages.sh
@@ -3,9 +3,11 @@
 cd ../config-consumer-service
 m3ast #mvn clean package
 
-docker image rm -f ericm24/config-consumer-service:k8.1
-docker image build -t ericm24/config-consumer-service:k8.1 .
-docker push ericm24/config-consumer-service:k8.1
+#docker image rm -f ericm24/config-consumer-service:k8.1
+#docker image build -t ericm24/config-consumer-service:k8.1 .
+#docker push ericm24/config-consumer-service:k8.1
+
+docker push ericm24/config-consumer-service:0.0.5-SNAPSHOT
 
 # Gets messed up with .envrc  Send params...
 #mkImagePush config-consumer-service:k8.1 ericm24
@@ -16,6 +18,8 @@ m3ast #mvn clean package
 docker image rm -f ericm24/config-service:k8.1
 docker image build -t ericm24/config-service:k8.1 .
 docker push ericm24/config-service:k8.1
+
+
 
 # Gets messed up with .envrc  Send params...
 #mkImagePush config-service:k8.1 ericm24
