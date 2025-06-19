eb10ae507a09573947f5c8d306c1ef2616b87c1d
ericm48 <me_manley@yahoo.com>
2025-06-19 13:19:43 -0600

    -Incremental commit, reset ports.

--- scripts/buildAndPushImages.sh
+++ scripts/buildAndPushImages.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
 cd ../config-consumer-service
-m3ast #mvn clean package
+m3mkpush # mvn clean package, build-img
 
-#docker image rm -f ericm24/config-consumer-service:k8.1
-#docker image build -t ericm24/config-consumer-service:k8.1 .
-#docker push ericm24/config-consumer-service:k8.1
+docker push $MK_IMAGE_PUSH_TARGET_PREFIX/$MK_IMAGE_PUSH_TARGET
 
-docker push ericm24/config-consumer-service:0.0.5-SNAPSHOT
 
-# Gets messed up with .envrc  Send params...
-#mkImagePush config-consumer-service:k8.1 ericm24
+#cd ../config-consumer-service
+#m3ast #mvn clean package
 
-cd ../config-consumer-service
-m3ast #mvn clean package
-
-docker image rm -f ericm24/config-service:k8.1
-docker image build -t ericm24/config-service:k8.1 .
-docker push ericm24/config-service:k8.1
+#docker image rm -f ericm24/config-service:k8.1
+#docker image build -t ericm24/config-service:k8.1 .
+#docker push ericm24/config-service:k8.1
 
 
 
